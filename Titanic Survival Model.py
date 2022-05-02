c8a8dcc26d6e425aca19871694c526e93a457cca
Önder Ekrem Tanrıkulu <onderemtan@gmail.com>
2022-05-02 17:18:27 +0200

    Update Titanic Survival Model.py

--- Titanic Survival Model.py
+++ Titanic Survival Model.py
@@ -7,8 +7,8 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report, plot_roc_curve
 from sklearn.model_selection import train_test_split, cross_validate
-from helpers_tr.eda import *
-from helpers_tr.data_prep import *
+from helpers.eda import *
+from helpers.data_prep import *
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
@@ -49,7 +49,7 @@ accuracy_score(y_pred, y_test)
 # 2. Model - Logistic Regression
 #############################################
 
-# Bağımlı ve bağımsız değişkelerin seçilmesi:
+# Selection of dependent and independent variables:
 y = df_prepared["SURVIVED"]
 X = df_prepared.drop(["PASSENGERID", "SURVIVED"], axis=1)
 
@@ -65,16 +65,16 @@ log_model.intercept_
 log_model.coef_
 
 
-# Tahmin
+# Prediction
 y_pred = log_model.predict(X)
 
 y_pred[0:10]
 y[0:10]
 
-# Test setinin modele sorulması:
+# pred with test set:
 y_pred = log_model.predict(X_test)
 
-# AUC Score için y_prob (1. sınıfa ait olma olasılıkları)
+# y_prob for AUC score (probability of belong to 1. class)
 y_prob = log_model.predict_proba(X_test)[:, 1]
 
 # ROC Curve
